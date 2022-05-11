07b9282f7580a1b98714a093cbd7449c4e926b54
codewithshamim <shamimislamonline@gmail.com>
2022-05-11 16:23:16 +0600

    home done

--- src/components/Home/Info.js
+++ src/components/Home/Info.js
@@ -9,34 +9,34 @@ const Info = () => {
       {/* _____________card_____________ */}
       <div className="card-container flex flex-col lg:flex-row mx-4 lg:mx-12 text-white text-left w-100">
         {/* ___________card1______________ */}
-        <div class="card flex lg:card-side shadow-xl bg-gradient-to-r from-secondary to-primary flex-1 md:content-center md:items-center">
+        <div className="card flex lg:card-side shadow-xl bg-gradient-to-r from-secondary to-primary flex-1 md:content-center md:items-center">
           <figure>
             <img src={clock} className="mt-2 md:mt-6 lg:m-8" alt="Album" />
           </figure>
-          <div class="card-body">
-            <h2 class="card-title">Opening Hours</h2>
+          <div className="card-body">
+            <h2 className="card-title">Opening Hours</h2>
             <p>Lorem Ipsum is simply dummy text of the pri.</p>
           </div>
         </div>
 
         {/* ___________card2______________ */}
-        <div class="card flex lg:card-side shadow-xl mx-0 my-4 lg:my-0 lg:mx-6 bg-accent flex-1 md:content-center md:items-center">
+        <div className="card flex lg:card-side shadow-xl mx-0 my-4 lg:my-0 lg:mx-6 bg-accent flex-1 md:content-center md:items-center">
           <figure>
             <img src={marker} className="mt-2 md:mt-6 lg:m-8" alt="Album" />
           </figure>
-          <div class="card-body">
-            <h2 class="card-title">Visit our location</h2>
+          <div className="card-body">
+            <h2 className="card-title">Visit our location</h2>
             <p>Brooklyn, NY 10036, United States.</p>
           </div>
         </div>
 
         {/* ___________card3______________ */}
-        <div class="card flex lg:card-side shadow-xl bg-gradient-to-r from-secondary to-primary md:content-center md:items-center flex-1">
+        <div className="card flex lg:card-side shadow-xl bg-gradient-to-r from-secondary to-primary md:content-center md:items-center flex-1">
           <figure>
             <img src={phone} className="mt-2 md:mt-6 lg:m-8" alt="Album" />
           </figure>
-          <div class="card-body">
-            <h2 class="card-title">Contact us now</h2>
+          <div className="card-body">
+            <h2 className="card-title">Contact us now</h2>
             <p>+000 123 456789.</p>
           </div>
         </div>
