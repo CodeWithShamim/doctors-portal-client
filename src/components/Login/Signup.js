3e59117ee582f46322f95d81dee9c7fc635dcaef
codewithshamim <shamimislamonline@gmail.com>
2022-05-13 18:38:23 +0600

    show error for sign up form

--- src/components/Login/Signup.js
+++ src/components/Login/Signup.js
@@ -2,8 +2,14 @@ import React from "react";
 import auth from "../../firebase.init";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
   const navigate = useNavigate();
 
@@ -20,6 +26,10 @@ const Login = () => {
     console.log(error);
   }
 
+  // ________use react hook form_________
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="flex items-center justify-center h-screen font-serif mx-4 md:mx-0">
       {/* _______card start__________ */}
@@ -27,30 +37,106 @@ const Login = () => {
         {/* ________card body start_______ */}
         <div className="card-body items-center text-left">
           <h2 className="card-title">Register</h2>
-          <form className="grid w-full">
+          <form onSubmit={handleSubmit(onSubmit)} className="grid w-full">
             <label htmlFor="name">Name</label>
             <input
-              className="border-2 rounded-lg p-2 mb-3"
+              className="border-2 rounded-lg p-2"
               type="name"
+              {...register("name", {
+                required: {
+                  value: true,
+                  message: "Name is required",
+                },
+                maxLength: {
+                  value: 20,
+                  message: "Name should be maximum 20 characters",
+                },
+              })}
               name="name"
               id="name"
             />
+            {/* Show error meassage for email___________ */}
+            <label className="mb-3">
+              {errors.name?.type === "required" && (
+                <span className="text-red-500 text-sm">
+                  {errors.name.message}
+                </span>
+              )}
+              {errors.name?.type === "maxLength" && (
+                <span className="text-red-500 text-sm">
+                  {errors.name.message}
+                </span>
+              )}
+            </label>
+            {/* _______name field end__________  */}
 
+            {/* ____email field start_____  */}
             <label htmlFor="email">Email</label>
             <input
-              className="border-2 rounded-lg p-2 mb-3"
+              className="border-2 rounded-lg p-2"
               type="email"
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Email is required",
+                },
+                pattern: {
+                  value:
+                    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+
+                  message: "Provide a valid email",
+                },
+              })}
               name="email"
               id="email"
             />
+            {/* Show error meassage for email___________ */}
+            <label className="mb-3">
+              {errors.email?.type === "required" && (
+                <span className="text-red-500 text-sm">
+                  {errors.email.message}
+                </span>
+              )}
+              {errors.email?.type === "pattern" && (
+                <span className="text-red-500 text-sm">
+                  {errors.email.message}
+                </span>
+              )}
+            </label>
+            {/* _____email field end______  */}
 
+            {/* _________password field start_________  */}
             <label htmlFor="password">Password</label>
             <input
               className="border-2 rounded-lg p-2"
               type="password"
+              {...register("password", {
+                required: {
+                  value: true,
+                  message: "Password is required",
+                },
+                maxLength: {
+                  value: 8,
+                  message: "Password should be maximum 8 characters",
+                },
+              })}
               name="password"
               id="password"
             />
+            {/* Show error meassage for password___________ */}
+            <label className="mb-3">
+              {errors.password?.type === "required" && (
+                <span className="text-red-500 text-sm">
+                  {errors.password.message}
+                </span>
+              )}
+              {errors.password?.type === "maxLength" && (
+                <span className="text-red-500 text-sm">
+                  {errors.password.message}
+                </span>
+              )}
+            </label>
+            {/* _________password field end_________  */}
 
             <span className="mb-4 text-xs p-2 cursor-pointer">
               Forgotton password?
