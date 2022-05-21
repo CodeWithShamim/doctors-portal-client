cb877be7c62d2ff79a1838e335070814327f8411
codewithshamim <shamimislamonline@gmail.com>
2022-05-21 22:46:59 +0600

    done

--- src/components/Appoinments/BookingModal.js
+++ src/components/Appoinments/BookingModal.js
@@ -5,147 +5,155 @@ import { toast } from "react-toastify";
 import auth from "../../firebase.init";
 
 const BookingModal = ({ treatment, setTreatment, date, refetch }) => {
-    const { _id, name, slots } = treatment;
-    const [{ displayName, email }] = useAuthState(auth);
+  const { _id, name, slots } = treatment;
+  const [{ displayName, email }] = useAuthState(auth);
 
-    // console.log(user);
+  // console.log(user);
 
-    // __________handleBooking__________
-    const handleBooking = (e) => {
-        e.preventDefault();
-        const date = e.target.date.value;
-        const slot = e.target.slot.value;
-        const phone = e.target.phone.value;
+  // __________handleBooking__________
+  const handleBooking = (e) => {
+    e.preventDefault();
+    const date = e.target.date.value;
+    const slot = e.target.slot.value;
+    const phone = e.target.phone.value;
 
-        const booking = {
-            treatmentId: _id,
-            treatmentName: name,
-            date: date,
-            slot: slot,
-            patientName: displayName,
-            patientPhone: phone,
-            patientEmail: email,
-        };
-
-        // console.log(booking);
-
-        fetch("https://doctors-portal100.herokuapp.com/booking", {
-                method: "POST",
-                headers: {
-                    "content-type": "application/json",
-                },
-                body: JSON.stringify(booking),
-            })
-            .then((res) => res.json())
-            .then((data) => {
-                // console.log(data);
-                // ____to close the modal___
-                if (data.insertedId) {
-                    refetch();
-                    setTreatment(null);
-                    toast.success(`Booking set is ${date} at ${slot}`);
-                }
-                if (data.success === "duplicate booking") {
-                    setTreatment(null);
-                    toast.error(
-                        `Already have a booking for ${data.booking.treatmentName} on ${date} at ${slot}`
-                    );
-                }
-            });
-
-        // _________reset form____________
-
-        const bookingForm = document.getElementById("booking-form");
-        bookingForm.reset();
+    const booking = {
+      treatmentId: _id,
+      treatmentName: name,
+      date: date,
+      slot: slot,
+      patientName: displayName,
+      patientPhone: phone,
+      patientEmail: email,
     };
 
-    return ( <
-        div > { /* _________modal start________ */ }
+    // console.log(booking);
 
-        <
-        input type = "checkbox"
-        id = "booking-modal"
-        className = "modal-toggle" / >
-        <
-        div className = "modal modal-top sm:modal-middle" >
-        <
-        div className = "modal-box" >
-        <
-        label htmlFor = "booking-modal"
-        className = "btn btn-sm btn-circle absolute right-2 top-2" >
-        ✕
-        <
-        /label>
+    fetch("https://doctors-portal100.herokuapp.com/booking", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(booking),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        // ____to close the modal___
+        if (data.insertedId) {
+          refetch();
+          setTreatment(null);
+          toast.success(`Booking set is ${date} at ${slot}`);
+        }
+        if (data.success === "duplicate booking") {
+          setTreatment(null);
+          toast.error(
+            `Already have a booking for ${data.booking.treatmentName} on ${date} at ${slot}`
+          );
+        }
+      });
 
-        { /* ______content Start________ */ } <
-        h3 className = "font-bold text-2xl text-left pb-6 text-secondary" > { name } <
-        /h3>
+    // _________reset form____________
 
-        { /* ______form_____ */ } <
-        form onSubmit = { handleBooking }
-        id = "booking-form"
-        className = "input-group flex flex-col" >
-        <
-        input className = "mt-6 border-0 rounded-lg p-2 outline-0 bg-base-200"
-        type = "text"
-        name = "date"
-        id = "date"
-        readOnly disabled value = { format(date, "PP") }
-        />
+    const bookingForm = document.getElementById("booking-form");
+    bookingForm.reset();
+  };
 
-        { /* ______select slots______ */ } <
-        select className = "border-2 border-base-300 select w-full my-6 rounded-lg p-2 outline-0"
-        name = "slot"
-        id = "slot"
-        required >
-        {
-            slots.map((slot, tabIndex) => ( <
-                option key = { tabIndex }
-                value = { slot } > { slot } <
-                /option>
-            ))
-        } <
-        /select>
+  return (
+    <div>
+      {/* _________modal start________ */}
 
-        <
-        input className = "border-2 rounded-lg p-2 outline-0 bg-base-200"
-        type = "text"
-        name = "name"
-        id = "name"
-        value = { displayName }
-        disabled readOnly placeholder = "Full Name" /
-        >
+      <input type="checkbox" id="booking-modal" className="modal-toggle" />
+      <div className="modal modal-top sm:modal-middle">
+        <div className="modal-box">
+          <label
+            htmlFor="booking-modal"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
 
-        <
-        input className = "my-6 border-2 rounded-lg p-2 outline-0"
-        type = "number"
-        name = "phone"
-        id = "phone"
-        required autoComplete = "off"
-        placeholder = "Phone Number" /
-        >
+          {/* ______content Start________ */}
+          <h3 className="font-bold text-2xl text-left pb-6 text-secondary">
+            {name}
+          </h3>
 
-        <
-        input className = "mb-6 border-2 rounded-lg p-2 outline-0 bg-base-200"
-        type = "email"
-        name = "email"
-        id = "email"
-        value = { email }
-        disabled readOnly placeholder = "Email" /
-        >
+          {/* ______form_____ */}
+          <form
+            onSubmit={handleBooking}
+            id="booking-form"
+            className="input-group flex flex-col"
+          >
+            <input
+              className="mt-6 border-0 rounded-lg p-2 outline-0 bg-base-200"
+              type="text"
+              name="date"
+              id="date"
+              readOnly
+              disabled
+              value={format(date, "PP")}
+            />
 
-        { /* ______submit btn_______ */ } <
-        input type = "submit"
-        value = "Submit"
-        htmlFor = "booking-modal"
-        className = "btn btn-secondary text-white font-bold w-full mx-auto" /
-        >
-        <
-        /form> { /* ______content End */ } <
-        /div> <
-        /div> { /* __________modal end__________ */ } <
-        /div>
-    );
+            {/* ______select slots______ */}
+            <select
+              className="border-2 border-base-300 select w-full my-6 rounded-lg p-2 outline-0"
+              name="slot"
+              id="slot"
+              required
+            >
+              {slots.map((slot, tabIndex) => (
+                <option key={tabIndex} value={slot}>
+                  {slot}
+                </option>
+              ))}
+            </select>
+
+            <input
+              className="border-2 rounded-lg p-2 outline-0 bg-base-200"
+              type="text"
+              name="name"
+              id="name"
+              value={displayName}
+              disabled
+              readOnly
+              placeholder="Full Name"
+            />
+
+            <input
+              className="my-6 border-2 rounded-lg p-2 outline-0"
+              type="number"
+              name="phone"
+              id="phone"
+              required
+              autoComplete="off"
+              placeholder="Phone Number"
+            />
+
+            <input
+              className="mb-6 border-2 rounded-lg p-2 outline-0 bg-base-200"
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              disabled
+              readOnly
+              placeholder="Email"
+            />
+
+            {/* ______submit btn_______ */}
+            <input
+              type="submit"
+              value="Submit"
+              htmlFor="booking-modal"
+              className="btn btn-secondary text-white font-bold w-full mx-auto"
+            />
+          </form>
+          {/* ______content End */}
+        </div>
+      </div>
+      {/* __________modal end__________ */}
+    </div>
+  );
 };
 
 export default BookingModal;
--- src/components/Dashboard/MyAppoinments.js
+++ src/components/Dashboard/MyAppoinments.js
@@ -6,80 +6,76 @@ import auth from "../../firebase.init";
 import Loading from "../Shared/Loading";
 
 const MyAppoinments = () => {
-    const [{ email }] = useAuthState(auth);
-    const [appoinments, setAppoinments] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-        const url = `https://doctors-portal100.herokuapp.com/booking?email=${email}`;
-        fetch(url, {
-                method: "GET",
-                headers: {
-                    authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-                },
-            })
-            .then((res) => res.json())
-            .then((data) => {
-                // _____handle error_______
-                if (
-                    data.message === "Forbidden access" ||
-                    data.message === "Unauthorized access"
-                ) {
-                    toast(data.message);
-                    localStorage.removeItem("accessToken");
-                    signOut(auth);
-                } else {
-                    setAppoinments(data);
-                }
+  const [{ email }] = useAuthState(auth);
+  const [appoinments, setAppoinments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const url = `https://doctors-portal100.herokuapp.com/booking?email=${email}`;
+    fetch(url, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // _____handle error_______
+        if (
+          data.message === "Forbidden access" ||
+          data.message === "Unauthorized access"
+        ) {
+          toast(data.message);
+          localStorage.removeItem("accessToken");
+          signOut(auth);
+        } else {
+          setAppoinments(data);
+        }
 
-                // ____loading end_____
-                setIsLoading(false);
-            });
-    }, [email]);
+        // ____loading end_____
+        setIsLoading(false);
+      });
+  }, [email]);
 
-    if (isLoading) {
-        return <Loading / > ;
-    }
+  if (isLoading) {
+    return <Loading />;
+  }
 
-    return ( <
-        div className = "rounded-lg p-6" >
-        <
-        div className = "overflow-x-auto" >
-        <
-        table className = "table w-full mx-auto" > { /* -- head -- */ } <
-        thead >
-        <
-        tr >
-        <
-        th > < /th> <
-        th > Name < /th> <
-        th > Phone Number < /th> <
-        th > Date < /th> <
-        th > Time < /th> <
-        th > Treatment Name < /th> <
-        /tr> <
-        /thead> <
-        tbody > {
-            appoinments ? .map(
-                ({ patientName, patientPhone, treatmentName, date, slot },
-                    index
-                ) => ( <
-                    tr key = { index } >
-                    <
-                    th > { index + 1 } < /th> <
-                    td > { patientName } < /td> <
-                    td > { patientPhone } < /td> <
-                    td > { date } < /td> <
-                    td > { slot } < /td> <
-                    td > { treatmentName } < /td> <
-                    /tr>
-                )
-            )
-        } <
-        /tbody> <
-        /table> <
-        /div> <
-        /div>
-    );
+  return (
+    <div className="rounded-lg p-6">
+      <div className="overflow-x-auto">
+        <table className="table w-full mx-auto">
+          {/* -- head -- */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Phone Number</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Treatment Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {appoinments?.map(
+              (
+                { patientName, patientPhone, treatmentName, date, slot },
+                index
+              ) => (
+                <tr key={index}>
+                  <th>{index + 1}</th>
+                  <td>{patientName}</td>
+                  <td>{patientPhone}</td>
+                  <td>{date}</td>
+                  <td>{slot}</td>
+                  <td>{treatmentName}</td>
+                </tr>
+              )
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyAppoinments;
